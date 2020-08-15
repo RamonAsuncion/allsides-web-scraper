907dbdaa44bb49ec3c6b679345bf8dda8d6f9d4f
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-14 22:38:22 -0400

    testing on website before doing on other sites

--- IdeaWebscrapper.py
+++ IdeaWebscrapper.py
@@ -1,2 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+
+source = requests.get('https://ramonasuncion.me/')
+
+
+def saveSource(html, path):
+    with open(path, 'wb') as f:
+        f.write(html)
+
+
+saveSource(r.content, 'ramonasuncion_me')
