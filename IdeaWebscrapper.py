f75c42c0d7b0b8759b6ecdd212b7c669fbb52413
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-15 23:31:32 -0400

    added so it links to specific news + rows

--- IdeaWebscrapper.py
+++ IdeaWebscrapper.py
@@ -5,10 +5,20 @@ url = 'https://www.allsides.com/media-bias/media-bias-ratings'
 
 source = requests.get(url)
 
-soup = BeautifulSoup(source, 'lxml')
+soup = BeautifulSoup(source.content, 'lxml')
 
-print(source.content[:50])  # first 50 characters of the source code
 
+print(source.content[:50])  # first 50 characters of the source code to test
+
+rows = soup.select('tbody tr')
+
+row = rows[0]
+
+
+linkToNewsInfo = row.select_one('.source-title a')['href']
+linkToNewsInfo = 'https://www.allsides.com' + linkToNewsInfo
+
+print(websitePage)
 
 # def saveSource(html, path):
 #     with open(path, 'wb') as f:
