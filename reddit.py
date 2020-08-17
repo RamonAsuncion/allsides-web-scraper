a74a809afe3a4cde8179710431bc636f5f721e55
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-17 11:37:27 -0400

    rename

--- reddit.py
+++ reddit.py
@@ -13,9 +13,10 @@ soup = BeautifulSoup(source.content, 'lxml')
 # first 50 characters of the source code to test
 print(source.content[:50])
 
+# Contains News source name, community feedback, link to news source, and bias data.
 fullTable = soup.select('tbody tr')
 
-# selects the first row s
+# selects the first row
 row = fullTable[0]
 
 # selects the news name of the row its targeting
@@ -31,10 +32,10 @@ linkToNewsInfo = 'https://www.allsides.com' + linkToNewsInfo
 # prints out the link to the news source information section
 print(linkToNewsInfo)
 
-
 biasCheck = row.select_one('.views-field-field-bias-image a')['href']
 
-biasCheck = biasCheck.split('/')[-1]  # try to understand why its -1
+# splits whats to the left of the /
+biasCheck = biasCheck.split('/')[-1]
 
-
+# prints out if the source is left, left-center, right, right center, center...
 print(biasCheck)
