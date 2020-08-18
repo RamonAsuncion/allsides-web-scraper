4974aa1b1ba672f1630a13458c7b998910ab56fd
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-17 22:49:15 -0400

    working on importing for other files

---
+++ communityFeedback.py
@@ -0,0 +1,10 @@
+# js file https://www.allsides.com/sites/default/files/js/js_RkhuUtuGis_wrzoAtkhY74JegSNy1LhoCkjSMXavCTI.js
+
+def communityVote(ratio):
+    if ratio > 3: return ""
+
+    elif 2 < ratio <= 3: return ""
+
+    elif 1.5 < ratio <= 2 return ""
+
+    elif 
