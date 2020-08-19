d9bb8456998a90aebddf609c83c33b025a0f15a1
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-18 23:02:11 -0400

    completed the ratio agreeance

--- communityFeedback.py
+++ communityFeedback.py
@@ -1,10 +1,32 @@
 # js file https://www.allsides.com/sites/default/files/js/js_RkhuUtuGis_wrzoAtkhY74JegSNy1LhoCkjSMXavCTI.js
 
 def communityVote(ratio):
-    if ratio > 3: return ""
+    if ratio > 3:
+        return "absolutely agrees"
 
-    elif 2 < ratio <= 3: return ""
+    elif 2 < ratio <= 3:
+        return "strongly agrees"
 
-    elif 1.5 < ratio <= 2 return ""
+    elif 1.5 < ratio <= 2:
+        return "agrees"
 
-    elif 
+    elif 1 < ratio <= 1.5:
+        return "somewhat agrees"
+
+    elif 0.67 < ratio < 1:
+        return "somewhat disagrees"
+
+    elif 0.5 < ratio <= 0.67:
+        return "disagrees"
+
+    elif 0.33 < ratio <= 0.5:
+        return "strongly disagrees"
+
+    elif ratio <= .33:
+        return "absolutely disagrees"
+
+    elif ratio == 1:
+        return "neutral"
+
+    else:
+        return None
