778da5ef34465ca2bae177ed9f397be2b983cbe4
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-18 23:46:42 -0400

    capitalize

--- communityFeedback.py
+++ communityFeedback.py
@@ -3,31 +3,31 @@
 def communityVote(ratio):
     # the value of the ratio goes through this if statement and prints out the return string
     if ratio > 3:
-        return "absolutely agrees"
+        return "Absolutely agrees"
 
     elif 2 < ratio <= 3:
-        return "strongly agrees"
+        return "Strongly agrees"
 
     elif 1.5 < ratio <= 2:
-        return "agrees"
+        return "Agrees"
 
     elif 1 < ratio <= 1.5:
-        return "somewhat agrees"
+        return "Somewhat agrees"
 
     elif 0.67 < ratio < 1:
-        return "somewhat disagrees"
+        return "Somewhat disagrees"
 
     elif 0.5 < ratio <= 0.67:
-        return "disagrees"
+        return "Disagrees"
 
     elif 0.33 < ratio <= 0.5:
-        return "strongly disagrees"
+        return "Strongly disagrees"
 
     elif ratio <= .33:
-        return "absolutely disagrees"
+        return "Absolutely disagrees"
 
     elif ratio == 1:
-        return "neutral"
+        return "Neutral"
 
     else:
         return None
