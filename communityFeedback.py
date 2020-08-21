0756fc04e8b4472726628ed8130ffbe6787b596b
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-20 23:03:31 -0400

    removed plural

--- communityFeedback.py
+++ communityFeedback.py
@@ -3,28 +3,28 @@
 def communityVote(ratio):
     # the value of the ratio goes through this if statement and prints out the return string
     if ratio > 3:
-        return "Absolutely agrees"
+        return "Absolutely agree"
 
     elif 2 < ratio <= 3:
-        return "Strongly agrees"
+        return "Strongly agree"
 
     elif 1.5 < ratio <= 2:
-        return "Agrees"
+        return "Agree"
 
     elif 1 < ratio <= 1.5:
-        return "Somewhat agrees"
+        return "Somewhat agree"
 
     elif 0.67 < ratio < 1:
-        return "Somewhat disagrees"
+        return "Somewhat disagree"
 
     elif 0.5 < ratio <= 0.67:
-        return "Disagrees"
+        return "Disagree"
 
     elif 0.33 < ratio <= 0.5:
-        return "Strongly disagrees"
+        return "Strongly disagree"
 
     elif ratio <= .33:
-        return "Absolutely disagrees"
+        return "Absolutely disagree"
 
     elif ratio == 1:
         return "Neutral"
