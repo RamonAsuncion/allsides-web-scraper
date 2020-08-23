4bf8656d1d72806b46ce9a5c65badd24d16c83a6
Ramon <asuncionbatista@gmail.com>
2020-08-22 23:17:41 -0400

    moved around code and added another for loop

--- allsidestables.py
+++ allsidestables.py
@@ -4,32 +4,39 @@ from communityFeedback import *
 from time import sleep
 import csv
 
-url = 'https://www.allsides.com/media-bias/media-bias-ratings'
-source = requests.get(url)
-soup = BeautifulSoup(source.content, 'lxml')
-table = soup.select('tbody tr')
-row = table[0]
-
 fullTable = []
 
-for row in table:
+pages = {
+    'https://www.allsides.com/media-bias/media-bias-ratings'
+}
 
-    f = dict()
+for url in pages:
 
-    f['newsName'] = row.select_one('.source-title').text.strip()
-    f['linkToNewsInfo'] = 'https://www.allsides.com' + \
-        row.select_one('.source-title a')['href']
-    f['bias'] = row.select_one(
-        '.views-field-field-bias-image a')['href'].split('/')[-1]
-    f['agree'] = int(row.select_one('.agree').text)
-    f['disagree'] = int(row.select_one('.disagree').text)
-    f['ratio'] = f['agree'] / f['disagree']
-    f['majorityCommunity'] = communityVote(f['ratio'])
+    source = requests.get(url)
+    soup = BeautifulSoup(source.content, 'lxml')
+    table = soup.select('tbody tr')
+    row = table[0]
 
-    fullTable.append(f)
+
+    for row in table:
+
+        f = dict()
+
+        f['newsName'] = row.select_one('.source-title').text.strip()
+        f['linkToNewsInfo'] = 'https://www.allsides.com' + \
+            row.select_one('.source-title a')['href']
+        f['bias'] = row.select_one(
+            '.views-field-field-bias-image a')['href'].split('/')[-1]
+        f['agree'] = int(row.select_one('.agree').text)
+        f['disagree'] = int(row.select_one('.disagree').text)
+        f['ratio'] = f['agree'] / f['disagree']
+        f['majorityCommunity'] = communityVote(f['ratio'])
+
+        fullTable.append(f)
 
 print(fullTable[1])
 
-sleep(10)
+sleep(10) # this is due to the ten seconds before request in robots.txt
+
 
 
