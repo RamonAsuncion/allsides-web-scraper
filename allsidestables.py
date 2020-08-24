e50ed0321158e95e9e37530906c869ae1725ac04
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-23 23:37:17 -0400

    renamed variables

--- allsidestables.py
+++ allsidestables.py
@@ -16,26 +16,22 @@ for url in pages:
     soup = BeautifulSoup(source.content, 'lxml')
     table = soup.select('tbody tr')
 
-
     for row in table:
 
         f = dict()
 
-        f['newsName'] = row.select_one('.source-title').text.strip()
+        f['News Source'] = row.select_one('.source-title').text.strip()
         f['linkToNewsInfo'] = 'https://www.allsides.com' + \
             row.select_one('.source-title a')['href']
-        f['bias'] = row.select_one(
+        f['AllSides Bias Rating'] = row.select_one(
             '.views-field-field-bias-image a')['href'].split('/')[-1]
         f['agree'] = int(row.select_one('.agree').text)
         f['disagree'] = int(row.select_one('.disagree').text)
-        f['ratio'] = f['agree'] / f['disagree']
-        f['majorityCommunity'] = communityVote(f['ratio'])
+        f['ratio'] = (f['agree'] / f['disagree'])
+        f['Community feedback'] = communityVote(f['ratio'])
 
         fullTable.append(f)
 
 print(fullTable[0])
 
-sleep(10) # this is due to the ten seconds before request in robots.txt
-
-
-
+sleep(10)  # this is due to the ten seconds before request in robots.txt
