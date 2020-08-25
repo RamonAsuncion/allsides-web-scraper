1c5619724d64fba3d557971edc1717083aaa1466
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-24 23:30:48 -0400

    put them into functions + WIP  progrss bar

--- allsidestables.py
+++ allsidestables.py
@@ -2,42 +2,56 @@ from bs4 import BeautifulSoup
 import requests
 from communityFeedback import *
 from time import sleep
-import csv
+from rich.progress import track
 
 fullTable = []  # empty list
 
 pages = [
     'https://www.allsides.com/media-bias/media-bias-ratings',
-    # found in source code line #2787 for ?page=1
+    # found in html source code line #2787 for ?page=1
     'https://www.allsides.com/media-bias/media-bias-ratings?page=1',
 ]
 
-for url in pages:
 
-    source = requests.get(url)
-    soup = BeautifulSoup(source.content, 'lxml')
-    table = soup.select('tbody tr')
+def table(fullTable):
+    for url in pages:
 
-    for row in table:
+        source = requests.get(url)
+        soup = BeautifulSoup(source.content, 'lxml')
+        table = soup.select('tbody tr')
 
-        f = dict()
+        for row in table:
 
-        f['News Source'] = row.select_one('.source-title').text.strip()
-        f['AllSides Bias Rating'] = row.select_one(
-            '.views-field-field-bias-image a')['href'].split('/')[-1]
-        f['linkToNewsInfo'] = 'https://www.allsides.com' + \
-            row.select_one('.source-title a')['href']
-        f['agree'] = int(row.select_one('.agree').text)
-        f['disagree'] = int(row.select_one('.disagree').text)
-        f['ratio'] = (f['agree'] / f['disagree'])
-        f['Community feedback'] = communityVote(f['ratio'])
-        f['ratio'] = "{:.3f}".format(f['ratio'])
+            f = dict()
 
-        # f['News Source Site'] =
+            f['News Source'] = row.select_one('.source-title').text.strip()
+            f['AllSides Bias Rating'] = row.select_one(
+                '.views-field-field-bias-image a')['href'].split('/')[-1]
+            f['linkToNewsInfo'] = 'https://www.allsides.com' + \
+                row.select_one('.source-title a')['href']
+            f['agree'] = int(row.select_one('.agree').text)
+            f['disagree'] = int(row.select_one('.disagree').text)
+            f['ratio'] = (f['agree'] / f['disagree'])
+            f['Community feedback'] = communityVote(f['ratio'])
+            f['ratio'] = "{:.3f}".format(f['ratio'])
 
-        fullTable.append(f)  # adds it to the empty list
+            fullTable.append(0)  # adds it to the empty list
+        sleep(10)  # this is due to the ten seconds before request in robots.txt
+    return fullTable
 
-print(fullTable[30])  # prints out the specific row
 
-sleep(10)  # this is due to the ten seconds before request in robots.txt
-print("10 seconds have passed")
+print("Ten seconds have passed; you can send another request.")
+
+
+def website(fullTable):
+    # Not all of them have website links
+    for d in track(range(100), description="Parsing..."):
+        r = requests.get(f['AllSides Bias Rating'])
+        soup = BeautifulSoup(source.content, 'lxml')
+
+        try:
+            ['News Source Site']
+        except TypeError:
+            pass
+
+    return fullTable
