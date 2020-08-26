1adb829a3f8b80fb313341f011ab4967b280bbc0
Ramon Asuncion <40077628+RamonAsuncion@users.noreply.github.com>
2020-08-25 22:43:09 -0400

    test

--- test.py
+++ test.py
@@ -9,8 +9,20 @@ soup = BeautifulSoup(source.content, 'lxml')
 
 table = soup.select('tbody tr')
 
-website = soup.select_one('.fa')['href']
-print(website)
+
+website = soup.select_one('a')['href']
+
+fullTable = []  # empty list
+
+for row in table:
+    f = dict()
+    f['AllSides Bias Rating'] = row.select_one(
+        '.views-field-field-bias-image a')['href'].split('/')[-1]
+
+    fullTable.append(f)
+
+print(fullTable[0])
+
 
 sleep(10)
 print("continue on testing 10 seconds have passed")
