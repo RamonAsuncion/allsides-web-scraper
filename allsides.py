941d40bb2cad82178760eb5efa6556c37b92f40f
Ramon Asuncion <asuncionbatista@gmail.com>
2020-09-08 09:51:35 -0400

    consistent variables

--- allsides.py
+++ allsides.py
@@ -11,16 +11,16 @@ page = [
 ]
 
 
-def table(fullTable):
+def table(full_table):
     # The main table
     print('Web scraper is parsing the table!')
     for url in page:
         source = requests.get(url)
         soup = BeautifulSoup(source.content, 'lxml')
 
-        table = soup.select('tbody tr')
+        main_table = soup.select('tbody tr')
 
-        for row in table:
+        for row in main_table:
 
             f = dict()  # dictionary
 
@@ -35,55 +35,56 @@ def table(fullTable):
             f['Community feedback'] = communityVote(f['Ratio'])
             f['Ratio'] = "{:.3f}".format(f['Ratio'])
 
-            fullTable.append(f)  # adds it to the empty list
+            full_table.append(f)  # adds it to the empty list
 
         sleep(10)  # this is due to the ten seconds before request in robots.txt
-    return fullTable
+        print('10')
+    return full_table
 
 
-def website(fullTable):
+def website(full_table):
     # Enters into the info page and parses out the info
-    for f in track(fullTable, description="Parsing..."):
+    for f in track(full_table, description="Parsing..."):
         source = requests.get(f['News Media Info'])
         soup = BeautifulSoup(source.content, 'lxml')
 
         try:
             # getting the website link to news source
-            website = soup.find('div', {'class': 'dynamic-grid'})
-            linkToNewsSource = website.find('a')['href']
-            f['News Source Site'] = linkToNewsSource
+            locate_html_class = soup.find('div', {'class': 'dynamic-grid'})
+            locate_paragraph = locate_html_class.find('a')['href']
+            f['News Source Site'] = locate_paragraph
         except TypeError:
             pass
         try:
             # getting the creation date of the news source
-            website = soup.find('div', {'class': 'dynamic-grid'})
-            paragraph = website.find_all('p')[1].text.split('.')[-1].strip()
-            f['Established'] = paragraph
+            locate__html_class = soup.find('div', {'class': 'dynamic-grid'})
+            locate_paragraph = locate__html_class.find_all('p')[1].text.split('.')[-1].strip()
+            f['Established'] = locate_paragraph
         except IndexError:
             pass
         try:
             # Who the news source owned by
-            website = soup.find('div', {'class': 'dynamic-grid'})
-            paragraph = website.find_all('p')[2].text.split(':')[-1].strip()
-            f['Owned by'] = paragraph
+            locate__html_class = soup.find('div', {'class': 'dynamic-grid'})
+            locate_paragraph = locate__html_class.find_all('p')[2].text.split(':')[-1].strip()
+            f['Owned by'] = locate_paragraph
         except IndexError:
             pass
         sleep(10)
-    return fullTable
+    return full_table
 
 
-def saving_data(fullTable):
+def saving_data(full_table):
     # Saves the data into a json file with no lines
     with open('all-sides.json', 'w', newline="") as i:
-        json.dump(fullTable, i)
+        json.dump(full_table, i)
 
 
 def main():
     # main function
-    fullTable = []  # empty list
-    fullTable = table(fullTable)
-    fullTable = website(fullTable)
-    saving_data(fullTable)
+    full_table = []  # empty list
+    full_table = table(full_table)
+    full_table = website(full_table)
+    saving_data(full_table)
 
     print('Parsing has finished!')
 
