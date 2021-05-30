e4caf1b6daac5918b6063da24d19ee5c7c76665e
Ramon Asuncion <asuncionbatista@gmail.com>
2021-05-30 14:50:23 -0400

    Removed dash from raiting. e.g. (left-center --> left center).

--- allsides.py
+++ allsides.py
@@ -5,32 +5,31 @@ from time import sleep
 from rich.progress import track
 import json
 
-page = ['https://www.allsides.com/media-bias/media-bias-ratings']
+# TODO: Finish progress bar. 
 
 
 def table(full_table):
     # the main table
     print('Web scraper is parsing the table!')
-    for url in page:
+    # only collects data from the first table that shows not the ones that load after the infinite scroll 
+    for url in ['https://www.allsides.com/media-bias/media-bias-ratings']:
         source = requests.get(url)
         soup = BeautifulSoup(source.content, 'lxml')
-
+        
         main_table = soup.select('tbody tr')
 
         for row in main_table:
-
+            
             f = dict()
 
             f['News Source'] = row.select_one('.source-title').text.strip()
-            f['AllSides Bias Rating'] = row.select_one(
-                '.views-field-field-bias-image a')['href'].split('/')[-1]
-            f['News Media Info'] = 'https://www.allsides.com' + \
-                row.select_one('.source-title a')['href']
+            f['AllSides Bias Rating'] = row.select_one('.views-field-field-bias-image a')['href'].split('/')[-1].replace("-"," ")
+            f['News Media Info'] = 'https://www.allsides.com' + row.select_one('.source-title a')['href']
             f['Agree'] = int(row.select_one('.agree').text)
             f['Disagree'] = int(row.select_one('.disagree').text)
             f['Ratio'] = (f['Agree'] / f['Disagree'])
             f['Community Feedback'] = community_vote(f['Ratio'])
-            f['Ratio'] = "{:.3f}".format(f['Ratio'])
+            f['Ratio'] = '{:.3f}'.format(f['Ratio'])
 
             full_table.append(f)  # adds it to the empty list
 
@@ -40,7 +39,7 @@ def table(full_table):
 
 def website(full_table):
     # enters into the info page and parses out the info
-    for f in track(full_table, description="Parsing..."):
+    for f in track(full_table, description='Parsing...'):
         source = requests.get(f['News Media Info'])
         soup = BeautifulSoup(source.content, 'lxml')
 
@@ -89,7 +88,7 @@ def website(full_table):
 
 def saving_data(full_table):
     # saves the data into a json file with no lines
-    with open('all-sides.json', 'w', newline="") as i:
+    with open('all-sides.json', 'w', newline=' ') as i:
         json.dump(full_table, i)
 
 
@@ -98,8 +97,6 @@ def main():
     full_table = table(full_table)
     full_table = website(full_table)
     saving_data(full_table)
-
-
 
 if __name__ == '__main__':
     main()
